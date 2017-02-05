16545b88039442f7657537525ba34033d4c3e5d4
Jaap <wijnen.jaap@gmail.com>
2017-02-05 13:00:40 +0100

    Initial commit

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOSGimma
+//
+//  Created by Jaap Wijnen on 05/02/2017.
+//  Copyright © 2017 Workmoose. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOSGimma
+//
+//  Created by Jaap Wijnen on 05/02/2017.
+//  Copyright © 2017 Workmoose. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
